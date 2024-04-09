f6b70e4fcbdad38cfe68e1d77cdb7539b906fa58
Thiago Ferreira <thiagodnf@gmail.com>
2024-04-09 15:54:51 -0400

    changed script.

--- run.sh
+++ run.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+
+DIRECTORY=backend/node_modules
+
+if [ ! -d "$DIRECTORY" ]; then
+  echo "$DIRECTORY does not exist."
+  cd backend
+  npm install
+  cd ..
+fi
+
 node ./backend/server.js
